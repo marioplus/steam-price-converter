c2aa3e143ee3ac4f9d5922b121cc63aa6d112c78
marioplus <nokiadafa@live.com>
2022-10-04 22:33:15 +0800

    update: 去掉获取汇率的锁

--- src/remote/ExchangeRateManager.ts
+++ src/remote/ExchangeRateManager.ts
@@ -24,8 +24,6 @@ class RateCache extends Serializable<RateCache> {
 
 export class ExchangeRateManager implements IRateApi {
 
-    private static refreshLock = false
-
     public static instance: ExchangeRateManager = new ExchangeRateManager()
     rateApis: Array<IRateApi> = new Array<IRateApi>()
     rateCache?: RateCache
@@ -46,12 +44,6 @@ export class ExchangeRateManager implements IRateApi {
         }
         // 过期需要重新获取
         if (this.rateCache.expired()) {
-            // 防止异步同时请求
-            if (ExchangeRateManager.refreshLock) {
-                await new Promise((resolve) => setTimeout(resolve, 50))
-            } else {
-                ExchangeRateManager.refreshLock = true
-            }
 
             // 两小时过期时间
             console.log('本地缓存已过期')
@@ -59,8 +51,6 @@ export class ExchangeRateManager implements IRateApi {
             this.rateCache.expiredAt = new Date().getTime() + (1000 * 60 * 60)
             this.saveRateCache(this.rateCache)
 
-            // 重置锁
-            ExchangeRateManager.refreshLock = true
         }
         return this.rateCache
     }
