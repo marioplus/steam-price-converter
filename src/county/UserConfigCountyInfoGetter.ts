5e5db38ac39de26ef02ec0ade9749b5cee06448f
marioplus <nokiadafa@live.com>
2024-12-09 22:13:40 +0800

    fix(): 应该使用“:”而不是“=”

--- src/county/UserConfigCountyInfoGetter.ts
+++ src/county/UserConfigCountyInfoGetter.ts
@@ -18,7 +18,7 @@ export class UserConfigCountyInfoGetter implements ICountyInfoGetter {
             // window.UserConfig={"logged_in":true,"steamid":"76561199160716481","accountid":1200450753,"is_support":false,"is_limited":false,"is_partner_member":false,"country_code":"CN"};
             document.querySelectorAll('script').forEach(scriptEl => {
                 const scriptInnerText = scriptEl.innerText
-                const regex: RegExp = /,"country_code"="([A-Z]{2})"/
+                const regex: RegExp = /,"country_code":"([A-Z]{2})"/
                 const match = regex.exec(scriptInnerText)
                 if (match) {
                     resolve(match[1])
