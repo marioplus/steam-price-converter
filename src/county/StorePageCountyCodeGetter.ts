9055f9fd493d828e45ffa73bd601748e20f2f148
marioplus <nokiadafa@live.com>
2025-06-30 22:56:28 +0800

    feat(2.5.5): 增加一种获取国家编码的方式

--- src/county/StorePageCountyCodeGetter.ts
+++ src/county/StorePageCountyCodeGetter.ts
@@ -26,6 +26,15 @@ export class StorePageCountyCodeGetter implements ICountyInfoGetter {
                 Logger.warn('读取商店页面区域代码变量失败： ' + e.message)
             }
 
+            // application_config 获取
+            const dataConfig = document.querySelector('div#application_config')?.getAttribute('data-config')
+            if (dataConfig) {
+                const countyCode = JSON.parse(dataConfig)?.['COUNTRY']
+                if (countyCode){
+                    resolve(countyCode)
+                }
+            }
+
             document.querySelectorAll('script').forEach(scriptEl => {
                 const scriptInnerText = scriptEl.innerText
                 if (scriptInnerText.includes('$J( InitMiniprofileHovers );') || scriptInnerText.includes(`$J( InitMiniprofileHovers( 'https%3A%2F%2Fstore.steampowered.com%2F' ) );`)) {
